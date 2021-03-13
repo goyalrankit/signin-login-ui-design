241b4f420becf525c2782a387e05550bcb149f3f
goyalrankit <goyalrankit@outlook.com>
2021-03-13 16:42:28 -0500

    Added useState hooks on Login Page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AnimationPageOpen from './AnimationPageOpen';
 
 const Login = () =>
 {
+
+    const [userEmail,setEmail] = useState();
+    const [userPassword, setUserPassword] = useState();
+
+
+    const inputEmailChange =(event) =>
+    {
+        setEmail(event.target.value);
+    }
+
+    const inputPasswordChange = (event) =>
+    {
+        setUserPassword(event.target.value);
+    }
+
+    const loginClicked =() =>
+    {
+       setEmail("");
+       setUserPassword(""); 
+       alert("Done");  
+    }
+
+
+
   return (
         <>
             <div className="signin-div">
                 <div className="form-part">
 
                 <div className="form">
-                  
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+
+                    <input type="email" 
+                           placeholder="Email"
+                           onChange = { inputEmailChange }
+                           value = {userEmail}
+
+                    />
+                    
+                    <input type="password" 
+                           placeholder="Password"
+                           onChange = { inputPasswordChange }
+                           value = {userPassword}
+                    />
+                                                
                         <div className="forget">
                             <small className="forget-small">Forgot Password? <span >RESET PASSWORD</span> </small>
                         </div>
-                    <button className="signin-button">LOGIN </button>
+                    <button className="signin-button" onClick={loginClicked}>LOGIN </button>
 
                     <div className="login">
                         <small>Don't have an account?  <span >SIGN IN</span> </small>
@@ -29,7 +64,7 @@ const Login = () =>
                     </div>
             </div>
         </>
-        )
-}
+        );
+};
 
 export default  Login;
